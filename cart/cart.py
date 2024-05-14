ed8164a4e1964a508c3337cd5024488ce765a5aa
margareth91 <margerytka@wp.eu>
2024-05-14 15:16:19 +0200

    added add method to class cart in cart app

--- cart/cart.py
+++ cart/cart.py
@@ -10,3 +10,17 @@ class Cart(object):
             # Zapis pustego koszyka na zakupy w sesji
             cart = self.session[settings.CART_SESSION_ID] = {}
         self.cart = cart
+
+    def add(self, product, quantity=1, update_quantity=False):
+        """Dodanie produktu do koszyka lub zmiana jego ilości"""
+        product_id = str(product.id)
+        if product_id not in self.cart:
+            self.cart[product_id] = {"quantity": 0, "price": str(product.price)}
+        if update_quantity:
+            self.cart[product_id]["quantity"] = quantity
+        else:
+            self.cart[product_id]["quantity"] += quantity
+        self.save()
+
+    def save(self):
+        pass
