ee2c193f79ff82a54941e413a6e657b7df55e30c
margareth91 <margerytka@wp.eu>
2024-05-14 15:39:09 +0200

    added iter method to class cart in cart app

--- cart/cart.py
+++ cart/cart.py
@@ -1,4 +1,8 @@
+from decimal import Decimal
+
 from django.conf import settings
+
+from shop.models import Product
 
 
 class Cart(object):
@@ -32,3 +36,17 @@ class Cart(object):
         if product_id in self.cart:
             del self.cart[product_id]
             self.save()
+
+    def __iter__(self):
+        """Iteracja przez elementy koszyka i pobranie produktów z bazy danych"""
+        product_ids = self.cart.keys()
+        # Pobranie obiektów produktów i dodanie ich do koszyka
+        products = Product.objects.filter(id__in=product_ids)
+
+        cart = self.cart.copy()
+        for product in products:
+            cart[str(product.id)]["product"] = product
+        for item in cart.values():
+            item["price"] = Decimal(item["price"])
+            item["total_price"] = item["price"] * item["quantity"]
+            yield item
