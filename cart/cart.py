d7d1ac194c6ce3a548ce235d6643666857aa4a98
margareth91 <margerytka@wp.eu>
2024-05-17 13:01:36 +0200

    changed update quantity from add to override in add method in cart model in cart app

--- cart/cart.py
+++ cart/cart.py
@@ -23,7 +23,7 @@ class Cart(object):
         if update_quantity:
             self.cart[product_id]["quantity"] = quantity
         else:
-            self.cart[product_id]["quantity"] += quantity
+            self.cart[product_id]["quantity"] = quantity
         self.save()
 
     def save(self):
