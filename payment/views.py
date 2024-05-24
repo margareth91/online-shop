48c6d153efb3b2ab426db9ce3a29111cdd289310
margareth91 <margerytka@wp.eu>
2024-05-24 13:51:27 +0200

    added payment done view to payment app views

--- payment/views.py
+++ payment/views.py
@@ -40,3 +40,7 @@ def payment_process(request):
             "payment/process.html",
             {"order": order, "client_token": client_token},
         )
+
+
+def payment_done(request):
+    return render(request, "payment/done.html")
