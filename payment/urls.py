2be4d2f010a998ca6b255d8a3629b7959ba63948
margareth91 <margerytka@wp.eu>
2024-05-24 13:53:10 +0200

    added payment cancelled url to payment app urls

--- payment/urls.py
+++ payment/urls.py
@@ -7,4 +7,5 @@ app_name = "payment"
 urlpatterns = [
     path("process/", views.payment_process, name="process"),
     path("done/", views.payment_done, name="done"),
+    path("cancelled/", views.payment_cancelled, name="cancelled"),
 ]
