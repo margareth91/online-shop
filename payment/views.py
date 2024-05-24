27e64f6ab55d6498ffc51072b3aeda02c2fdc979
margareth91 <margerytka@wp.eu>
2024-05-24 13:51:01 +0200

    added payment process view to payment app views

--- payment/views.py
+++ payment/views.py
@@ -1,3 +1,42 @@
-from django.shortcuts import render
+import braintree
+from django.conf import settings
+from django.shortcuts import render, redirect, get_object_or_404
 
-# Create your views here.
+from orders.models import Order
+
+# Utworzenie egzemplarza bramki płatności
+gateway = braintree.BraintreeGateway(settings.BRAINTREE_CONF)
+
+
+def payment_process(request):
+    order_id = request.session.get("order_id")
+    order = get_object_or_404(Order, id=order_id)
+
+    if request.method == "POST":
+        # Pobranie tokena
+        nonce = request.POST.get("payment_method_nonce", None)
+        # Utworzenie i przesłanie transakcji
+        result = braintree.Transaction.sale(
+            {
+                "amount": f"{order.get_total_cost():.2f}",
+                "payment_method_nonce": nonce,
+                "options": {"submit_for_settlement": True},
+            }
+        )
+        if result.is_success:
+            # Oznaczenie zamówienia jako opłacone
+            order.paid = True
+            # Zapisanie unikatowego identyfikatora transakcji
+            order.braintree_id = result.transaction.id
+            order.save()
+            return redirect("payment:done")
+        else:
+            return redirect("payment:cancelled")
+    else:
+        # Wygenerowanie tokena
+        client_token = braintree.ClientToken.generate()
+        return render(
+            request,
+            "payment/process.html",
+            {"order": order, "client_token": client_token},
+        )
