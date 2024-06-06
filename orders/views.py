5a67d1edb8fb4b2a2a3cf95b2d814e73d0c9dc26
margareth91 <margerytka@wp.eu>
2024-06-06 16:00:52 +0200

    added admin order detail ulr to orders app urls

--- orders/views.py
+++ orders/views.py
@@ -1,9 +1,10 @@
-from django.shortcuts import render, redirect
+from django.contrib.admin.views.decorators import staff_member_required
+from django.shortcuts import render, redirect, get_object_or_404
 from django.urls import reverse
 
 from cart.cart import Cart
 from .forms import OrderCreateForm
-from .models import OrderItem
+from .models import OrderItem, Order
 from .tasks import order_created
 
 
@@ -25,10 +26,16 @@ def order_create(request):
                 # Uruchomienie aynchronicznego zadania
                 order_created.delay(order.id)
                 # Umieszczenie zamówienia w sesji
-                request.session['order_id'] = order.id
+                request.session["order_id"] = order.id
                 # Przekierowanie do płatności
-                return redirect(reverse('payment:process'))
+                return redirect(reverse("payment:process"))
                 # return render(request, "orders/order/created.html", {"order": order})
     else:
         form = OrderCreateForm()
     return render(request, "orders/order/create.html", {"cart": cart, "form": form})
+
+
+@staff_member_required
+def admin_order_detail(request, order_id):
+    order = get_object_or_404(Order, id=order_id)
+    return render(request, "admin/orders/order/detail.html", {"order": order})
